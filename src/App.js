3eecf262c5dfa20968d026656d373e35fbc3634e
TienHunter <tienkbtnhp@gmail.com>
2022-01-02 19:27:25 +0700

    todoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs')) || [];
+    return storageJobs
+  })
+  const [id, setId] = useState();
+  const [edit, setEdit] = useState(false);
+  const handleSubmit = () => {
+    if (job) {
+      if (edit) {
+        handleUpdate();
+      } else {
+        setJobs(prev => {
+          const newJobs = [...prev, job];
+          localStorage.setItem('jobs', JSON.stringify(newJobs));
+          return newJobs
+        })
+      }
+    }
+    setJob('')
+  }
+  const handleUpdate = () => {
+    jobs[id] = job;
+    setEdit(false)
+    setJobs(() => {
+      localStorage.setItem('jobs', JSON.stringify(jobs));
+      return jobs;
+    })
+  }
+  const handleRemove = (index) => {
+    console.log(index);
+    setJobs(prev => {
+      const newJobs = prev.filter((item, id) => id !== index)
+      console.log(newJobs);
+      localStorage.setItem('jobs', JSON.stringify(newJobs));
+      return newJobs
+    })
+  }
+  const handleEdit = (index) => {
+    setEdit(true)
+    setJob(jobs[index]);
+    setId(index);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{ padding: 20 }}>
+      <input
+        value={job}
+        onChange={(e) => setJob(e.target.value)}
+        placeholder='Input your work'
+      />
+      <button onClick={handleSubmit}>{edit ? 'Change' : 'Add'}</button>
+      <ul>
+        {
+          jobs.map((job, index) => {
+
+            return (
+              <li key={index}>
+                {job}
+                <button onClick={() => handleRemove(index)}>Remove</button>
+                <button onClick={() => handleEdit(index)}>Edit</button>
+
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div >
   );
 }
 
