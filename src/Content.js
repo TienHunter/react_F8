6c155088ba6bd0d299814d43ee53cdc7e8a5f475
TienHunter <tienkbtnhp@gmail.com>
2022-01-06 23:03:45 +0700

    useEffect with preview avatar

--- src/Content.js
+++ src/Content.js
@@ -1,75 +1,26 @@
 import { useState, useEffect } from 'react'
-const tabs = ['posts', 'albums', 'todos']
+
 function Content() {
-   const [title, setTile] = useState('')
-   const [posts, setPosts] = useState([])
-   const [type, setType] = useState('posts')
-   const [showOnScroll, setShowOnScroll] = useState(false)
-   document.title = title
+   const [avatar, setAvatar] = useState()
    useEffect(() => {
-      fetch(`https://jsonplaceholder.typicode.com/${type}`)
-         .then(res => res.json())
-         .then(posts => {
-            setPosts(posts);
-         })
-   }, [type])
+      //cleanup
 
-   useEffect(() => {
-      const handleScroll = () => {
-         if (window.scrollY >= 200) {
-            //show
-            setShowOnScroll(true)
-         } else {
-            //Hide
-            setShowOnScroll(false)
-         }
-      }
-
-      window.addEventListener('scroll', handleScroll)
-
-      //cleanup function
-      return () => {
-         window.removeEventListener('scroll', handleScroll)
-      }
-   })
+      return () => { avatar && URL.revokeObjectURL(avatar.preview) }
+   }, [avatar])
+   const handlePreviewAvatar = (e) => {
+      const file = e.target.files[0]
+      file.preview = URL.createObjectURL(file)
+      setAvatar(file)
+   }
    return (
       <div>
-         {tabs.map(tab =>
-            <button
-               key={tab}
-               onClick={() => setType(tab)}
-               style={tab === type ? { background: '#ccc', color: '#fff' } : {}}
-            >
-               {tab}
-            </button>
-         )}
          <input
-            value={title}
-            onChange={e => setTile(e.target.value)}
+            type='file'
+            onChange={handlePreviewAvatar}
          />
-         <ul>
-            {posts.map(post =>
-               <li key={post.id}>{post.title}</li>
-            )}
-         </ul>
-
-         {showOnScroll &&
-            <button
-               onClick={() => {
-                  window.scrollTo(0, 0)
-                  setShowOnScroll(false)
-               }}
-               style={{
-                  position: 'fixed',
-                  right: 20,
-                  bottom: 20
-               }}
-            >
-
-               Go to top
-
-            </button>
-         }
+         {avatar && (
+            <img src={avatar.preview} alt='' width='80%' />
+         )}
       </div>
    )
 }
