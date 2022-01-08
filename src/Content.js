24eee1dd60fa19548a29c11d76ed2b9aeaea74cd
TienHunter <tienkbtnhp@gmail.com>
2022-01-08 20:54:28 +0700

    useRef hook

--- src/Content.js
+++ src/Content.js
@@ -1,46 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-const courses = [
-   {
-      id: 1,
-      name: 'HTML/CSS'
-   },
-   {
-      id: 2,
-      name: 'Javascript'
-   },
-   {
-      id: 3,
-      name: 'React'
-   }
-]
+
 function Content() {
-   const [courseId, setCourseId] = useState(1)
-
+   const [count, setCount] = useState(60)
+   const [start, setStart] = useState(false)
+   const timeId = useRef()
+   const prevCount = useRef()
    useEffect(() => {
-      const handleComment = ({ detail }) => {
-         console.log(detail);
+      prevCount.current = count
+   }, [count])
+   const handleStart = () => {
+      setStart(true)
+      if (!start) {
+         timeId.current = setInterval(() => {
+            setCount(prevCount => prevCount - 1)
+         }, 1000)
       }
+      console.log('start ->', timeId.current);
 
-      window.addEventListener(`course-${courseId}`, handleComment)
-
-      return () => {
-         window.removeEventListener(`course-${courseId}`, handleComment)
-      }
-   }, [courseId])
+   }
+   const handleStop = () => {
+      setStart(false)
+      clearInterval(timeId.current)
+      console.log('stop ->', timeId.current);
+   }
+   console.log(count, prevCount.current);
    return (
       <div>
-         <ul>
-            {courses.map((course) =>
-               <li
-                  key={course.id}
-                  style={{ color: courseId === course.id ? 'red' : '#ccc' }}
-                  onClick={() => setCourseId(course.id)}
-               >
-                  {course.name}
-               </li>
-            )}
-         </ul>
+         <h1>{count}</h1>
+         <button onClick={handleStart} > start </button>
+         <button onClick={handleStop} > stop </button>
       </div>
    )
 }
