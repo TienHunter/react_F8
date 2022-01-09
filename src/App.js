be8d54afe98fd553a4392230618239457a1b5315
TienHunter <tienkbtnhp@gmail.com>
2022-01-09 16:27:52 +0700

    todo App with useReducer() hook

--- src/App.js
+++ src/App.js
@@ -1,48 +1,105 @@
 
-import { useReducer } from 'react'
+import { useReducer, useRef } from 'react'
 
+// useReducer
 
-// init state
-const initState = 0
+// 1. init state
+const initState = {
+  job: '',
+  jobs: []
+}
+// 2. Actions
+const SET_JOB = 'set_job'
+const ADD_JOB = 'add_job'
+const DELETE_JOB = 'delete_job'
 
-// Action
-const UP_COUNT = 'up'
-const DOWN_COUNT = 'down'
-
-// reducer
-const reducer = (state, action) => {
-  console.log('reducer running ...');
-  switch (action) {
-    case UP_COUNT:
-      return state + 1;
-
-    case DOWN_COUNT:
-      return state - 1;
-
-    default:
-      throw new Error('Invald action');
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload
   }
 }
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+const deleteJob = (payload) => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+// 3.reducer
+const reducer = (state, action) => {
+  console.log('Action', action);
+  console.log('Prev state:', state);
+
+  let newState;
+  switch (action.type) {
+    case SET_JOB:
+      newState = {
+        ...state,
+        job: action.payload
+      }
+      break
+    case ADD_JOB:
+      newState = {
+        ...state,
+        jobs: [...state.jobs, action.payload]
+      }
+      break
+    case DELETE_JOB:
+      const newJobs = [...state.jobs]
+      newJobs.splice(action.payload, 1)
+      newState = {
+        ...state,
+        jobs: newJobs
+      }
+      break
+    default:
+      throw new Error('Invalid action !!!')
+  }
+  console.log('New state', newState);
+  return newState
+}
+
+// 4.dispatch
 
 function App() {
 
-  const [count, dispatch] = useReducer(reducer, initState)
+  const [state, dispatch] = useReducer(reducer, initState)
+  const { job, jobs } = state
 
+  const inputRef = useRef()
+  const hanldeSubmit = () => {
+    dispatch(addJob(job))
+    dispatch(setJob(''))
+    inputRef.current.focus()
+  }
   return (
-    <div style={{ padding: '20px' }}>
-      <h1> {count} </h1>
-      <button
-        onClick={() => dispatch(DOWN_COUNT)}
-      >
-        Down
-      </button>
-      <button
-        onClick={() => dispatch(UP_COUNT)}
-      >
-        Up
-      </button>
+    <div>
+      <h2>Todo</h2>
 
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder="Enter your work..."
+        onChange={(e) => dispatch(setJob(e.target.value))}
+      />
+
+      <button onClick={hanldeSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job, index) =>
+          <li key={index} >
+            {job}
+            <button onClick={() => dispatch(deleteJob(index))}>&times;</button>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
-export default App;
+export default App
