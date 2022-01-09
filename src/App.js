852f1729f4a58018dbe847bf52abd901da6e556c
TienHunter <tienkbtnhp@gmail.com>
2022-01-09 11:31:23 +0700

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import { useState, useCallback } from 'react';
-import Content from './Content.js';
+import { useState, useMemo, useRef } from 'react';
+// import Content from './Content.js';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const handleIncrease = useCallback(() => {
-    setCount(prev => prev + 1)
-  }, [])
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [products, setProducts] = useState([])
+  const inputName = useRef();
+
+  const hanldeSubmit = () => {
+    setProducts([...products, {
+      name: name,
+      price: +price
+    }])
+    setName('')
+    setPrice('')
+    inputName.current.focus()
+  }
+
+  const total = useMemo(() => {
+    products.reduce((total, product) => {
+      console.log('tinh toan lai ...');
+      return total + product.price;
+    }, 0)
+  }, [products])
+
   return (
-    <div className="App" style={{ padding: 20 }}>
-      <Content onIncrease={handleIncrease} />
-      <h1> {count} </h1>
-    </div >
-  );
+    <div>
+      <input
+        ref={inputName}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='Enter your name ...'
+      />
+      <br />
+      <input
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+        placeholder='Enter your price ...'
+      />
+      <br />
+      <button onClick={hanldeSubmit}>Add</button>
+      <br />Total: {total}<br />
+      <ul>
+        {products.map((product, index) =>
+          <li key={index} > {product.name} - {product.price} </li>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
